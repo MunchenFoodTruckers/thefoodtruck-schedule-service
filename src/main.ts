5c643630c9b5b06f687843ecf6cdf5834510dc56
San <sandeeppaul11111@gmail.com>
2025-11-23 12:47:54 +0100

    fix: bind to 0.0.0.0 for Render

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix("");
   const port = process.env.PORT || 3005;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Schedule service running on http://localhost:${port}`);
 }
 
